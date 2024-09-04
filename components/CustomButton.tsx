dbbdaac0a858ea61a762556b7e5d4bc68baac9a0
oleg turevici <ol.turevici@gmail.com>
2024-09-04 15:13:07 +0300

    footer add

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,16 +1,17 @@
 "use client";
 import Image  from 'next/image'
+import {CustomButtonProps} from "@/types";
 
-const CustomButton = () => {
+const CustomButton = ({title, containerStyles, handlClick, btnType}:CustomButtonProps) => {
     return (
         <button
         disabled={false}
-        type={"button"}
-        className={'custom-btn'}
-        onClick={()=>{}}
+        type={btnType||"button"}
+        className={`custom-btn ${containerStyles}`}
+        onClick={handlClick}
         >
           <span className={'flex-1'}>
-              title
+              {title}
           </span>
         </button>
     )
