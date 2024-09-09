3c54f271ec996597b85e457d2d55d2fb0d4c4e21
Voyager_n <ol.turevici@gmail.com>
2024-09-09 16:36:11 +0300

    autentification

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import {UserButton} from "@clerk/nextjs";
+
+const SetupPage=()=> {
+    return (
+        <div className="p-4">
+   <UserButton afterSignOutUrl="/"/>
+        </div>
+    );
+}
+export default SetupPage;
