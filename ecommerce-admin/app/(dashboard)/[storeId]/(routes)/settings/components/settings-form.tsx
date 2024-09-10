489faf3c31a28bc6c23a841fc112f75b9da3c5b3
Voyager_n <ol.turevici@gmail.com>
2024-09-10 22:47:40 +0300

    add copy and api url

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -15,6 +15,9 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import {useParams, useRouter} from "next/navigation";
 import {AlertModal} from "@/components/modals/alert-modal";
+import {ApiAlert} from "@/components/ui/api-alert";
+import {useOrigin} from "@/hooks/use-origin";
+
 
 interface SettingsFormProps {
     initialData: Store
@@ -32,6 +35,8 @@ export const SettingsForm: React.FC<SettingsFormProps>=({
                                                         }) => {
     const params = useParams();
     const router=useRouter()
+    const origin=useOrigin();
+
     const [open, setOpen] = useState(false)
     const [loading, setLoading] = useState(false)
 
@@ -117,6 +122,8 @@ export const SettingsForm: React.FC<SettingsFormProps>=({
                     </Button>
                 </form>
             </Form>
+            <Separator/>
+            <ApiAlert title="NEXT_PUBLIC_API_URL" description={`${origin}/api/${params.storeId}`} variant="public" />
             </>
     )
 }
